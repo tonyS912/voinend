63f281f159ca1ff0274bb06e60be8fa67eeabddb
Tonys <info@tony-schiller.com>
2023-09-11 07:25:35 +0200

    update models

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Contacts(models.Model):
+    firstname = models.CharField(max_length=40)
+    lastname = models.CharField(max_length=40)
+    email = models.EmailField(max_length=100)
+    phone = models.IntegerField()
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.utils.translation import gettext as _
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import Contacts
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+
+class Tasks(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(max_length=300)
+    assigned_to = models.ManyToManyField(Contacts, related_name="assignee", blank=True)
+    due_date = models.DateField(null=True)
+    priority = models.CharField(max_length=15, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    sub_tasks = models.JSONField(default=list, null=True)
+
+    status = models.CharField(max_length=20)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateField(_("Date"), default=datetime.date.today)
