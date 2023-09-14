1874d5077903084ba3a827cc15e746ed2862d345
Tonys <info@tony-schiller.com>
2023-09-14 20:06:00 +0200

    update static

--- backend/settings.py
+++ backend/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from django.contrib.staticfiles
+import django.contrib.staticfiles
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
