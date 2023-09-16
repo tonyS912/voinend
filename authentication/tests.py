f02f1d81b8caa3c89250f34f736ff737ffa40558
Tonys <info@tony-schiller.com>
2023-09-16 19:11:14 +0200

    update views und serializers

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,8 +1,8 @@
 from django.urls import reverse
+from django.test import TestCase, Client
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
 
 
 class AccountTests(APITestCase):
@@ -18,5 +18,15 @@ class AccountTests(APITestCase):
             "last_name": "Test"
         }
         response = self.client.post(url, data, format='json')
-        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
-        self.assertEquals(User.objects.get().username, 'Klaus')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.get().username, 'Klaus')
+
+
+class LoginTest(TestCase):
+    def test_login(self):
+        self.client = Client()
+        self.user = User.objects.create_user('test_user', email='test@testomat.com', password='test123!')
+        self.client.login(email='test@testomat.com', password='test123!')
+
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
